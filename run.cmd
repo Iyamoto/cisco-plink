c61cbd6e21115e215047a283e8a16338513116da
Iyamoto <Iyamoto@users.noreply.github.com>
2013-09-23 03:47:35 -0700

    Update run.cmd

--- run.cmd
+++ run.cmd
@@ -1,4 +1,7 @@
 @echo off
 set user=cisco
 set password=password
+rem Execute commands from a file
 for /f %%i in (iplist.txt) do plink.exe %user%@%%i -pw %password% < commands.txt >%%i.log
+rem Backup config
+for /f %%i in (iplist.txt) do plink.exe %user%@%%i -pw %password% "sh run" >%%i.config
