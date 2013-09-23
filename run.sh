244c9452359e8bbd8433101a605f3231b4418af3
Iyamoto <Iyamoto@users.noreply.github.com>
2013-09-23 03:12:30 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
+#Exmaple script
 
+#List of cisco devices IPs
 ips=`cat iplist.txt`
+#which plink
 plink="/usr/bin/plink"
 user="cisco"
 password="password"
 
 for ip in $ips
 do
-echo $ip
-$plink -l $user -pw $password $ip < commands.txt > $ip.log
+  echo $ip
+  #Execute command from a file
+  $plink -l $user -pw $password $ip < commands.txt > $ip.log
+  #Backup config
+  $plink -l $user -pw $password $ip "sh run" > $ip.config
 done
